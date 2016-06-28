da2af8398fb7e25d88979590829c16a1cdb577b5
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2016-06-28 22:35:43 +0300

    todo list

---
+++ public/components/Common/Button.js
@@ -0,0 +1,64 @@
+/**
+ * Created by stylehopper on 28.06.16.
+ */
+
+import React from 'react';
+import {
+  StyleSheet,
+  TouchableHighlight,
+  Text
+} from 'react-native';
+
+class Button extends React.Component{
+  constructor() {
+    super();
+
+    this.state = { active: false };
+
+    this._onHighlight = this._onHighlight.bind(this);
+    this._onUnhighlight = this._onUnhighlight.bind(this);
+  }
+
+  _onHighlight() {
+    this.setState({active: true});
+  }
+
+  _onUnhighlight() {
+    this.setState({active: false});
+  }
+
+  render() {
+    var colorStyle = {
+      color: this.state.active ? '#fff' : '#000',
+    };
+    return (
+      <TouchableHighlight
+        onHideUnderlay={this._onUnhighlight}
+        onPress={this.props.onPress}
+        onShowUnderlay={this._onHighlight}
+        style={[styles.button, this.props.style]}
+        underlayColor="#a9d9d4">
+        <Text style={[styles.buttonText, colorStyle]}>{this.props.children}</Text>
+      </TouchableHighlight>
+    );
+  }
+}
+
+var styles = StyleSheet.create({
+  button: {
+    borderRadius: 5,
+    flex: 1,
+    height: 44,
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+    // overflow: 'hidden',
+    backgroundColor: 'red'
+  },
+  buttonText: {
+    fontSize: 18,
+    margin: 5,
+    textAlign: 'center'
+  }
+});
+
+export default Button;
--- public/components/Todo/TodoList.js
+++ public/components/Todo/TodoList.js
@@ -5,14 +5,92 @@
 import React from 'react';
 import {
   View,
-  Text
+  Text,
+  ListView,
+  TouchableHighlight,
+  Stylesheet,
+  Alert
 } from 'react-native'
 
+import Button from './../Common/Button';
+
+const rows = [
+  { id: '1', title: 'Todo 1', description: 'I must do something' },
+  { id: '2', title: 'Todo 2', description: 'I must do something' },
+  { id: '3', title: 'Todo 3', description: 'I must do something' },
+  { id: '4', title: 'Todo 4', description: 'I must do something' },
+  { id: '5', title: 'Todo 5', description: 'I must do something' },
+  { id: '6', title: 'Todo 6', description: 'I must do something' }
+];
+var lastRowNr = 6;
+//
+// const styles = Stylesheet.create({
+//
+// });
+
+// const rowsTitles = rows.map(row => row.title);
+
 class TodoList extends React.Component {
+
+  constructor() {
+    super();
+
+    this.getListView = this.getListView.bind(this);
+    this.onPressRow = this.onPressRow.bind(this);
+    this.onPressStartAddTodo = this.onPressStartAddTodo.bind(this);
+
+    const ds = this.getListView();
+    this.state = { dataSource: ds.cloneWithRows(rows) };
+  }
+
+  getListView() {
+    return new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+  }
+
+  onPressRow(rid) {
+    // Alert.alert(
+    //   'Alert Title',
+    //   'My Alert Msg',
+    //   [
+    //     {text: 'Ask me later', onPress: () => console.log('Ask me later pressed')},
+    //     {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+    //     {text: 'OK', onPress: () => console.log('OK Pressed')},
+    //   ]
+    // )
+    rows.splice(rid, 1);
+    this.setState({ dataSource: this.getListView().cloneWithRows(rows)} );
+  }
+
+  onPressStartAddTodo() {
+    lastRowNr++;
+    rows.push({
+      id: lastRowNr,
+      title: `Todo ${lastRowNr}`,
+      description: 'I must do something'
+    });
+    this.setState({
+      dataSource: this.getListView().cloneWithRows(rows)
+    })
+  }
+
+  renderRow = (todo, sid, rid) => {
+    return (
+      <TouchableHighlight onPress={e => this.onPressRow(rid)}>
+        <Text>{todo.title}</Text>
+      </TouchableHighlight>
+    )
+  };
+
   render() {
     return (
       <View>
-        <Text>Todo List</Text>
+        <View>
+          <Button onPress={this.onPressStartAddTodo} />
+        </View>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this.renderRow}
+        />
       </View>
     )
   }
